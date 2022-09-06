e0e5f6a9e57fcae60f8e5b909125ea59c03661f0
Billy Hao <billyhao12@gmail.com>
2022-09-05 17:40:29 -0700

    Updates

---
+++ precache-manifest.463b7d46606b1d08ddc950a03c2abe95.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a1ebad4974fce9fb4593e2f27bd6afb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5289fe140d63cacef07c",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4cc01cdc0e2eec92c280",
+    "url": "/static/css/main.fcdc90dc.chunk.css"
+  },
+  {
+    "revision": "5289fe140d63cacef07c",
+    "url": "/static/js/2.8e7a771a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8e7a771a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cc01cdc0e2eec92c280",
+    "url": "/static/js/main.2d306631.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "dd3064fa3893f59a7c9174ee879313de",
+    "url": "/static/media/Billy.dd3064fa.jpg"
+  },
+  {
+    "revision": "ea5c7ab3da71e746c503cc59d63907cf",
+    "url": "/static/media/Burger-App-screenshot.ea5c7ab3.png"
+  },
+  {
+    "revision": "4db0fb0e2959a0dddf3c2165cc59f3bc",
+    "url": "/static/media/ChefPro-screenshot.4db0fb0e.png"
+  },
+  {
+    "revision": "8ad942a93173e4fd765c5084753b8d8f",
+    "url": "/static/media/Employee-Tracker-screenshot.8ad942a9.png"
+  },
+  {
+    "revision": "9589c855999a3ad2a248f7398280ac92",
+    "url": "/static/media/Team-Profile-Generator-screenshot.9589c855.png"
+  },
+  {
+    "revision": "fbab8197c7e690819ddeca4e17674aae",
+    "url": "/static/media/The-Nomad-screenshot.fbab8197.png"
+  },
+  {
+    "revision": "48d20242104dc15de56493d8d309b006",
+    "url": "/static/media/Weather-Event-Planner-screenshot.48d20242.png"
+  },
+  {
+    "revision": "722a06484a889c1027d5954f98212269",
+    "url": "/static/media/Work-Day-Scheduler-screenshot.722a0648.png"
+  },
+  {
+    "revision": "49c5679657ef2183199897f17893644c",
+    "url": "/static/media/resume.49c56796.pdf"
+  }
+]);
